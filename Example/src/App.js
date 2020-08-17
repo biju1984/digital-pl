536faefdc9633f32142ee12f131beb2d7d0e5aa6
Biju Joseph <moonwalker@Bijus-MacBook-Pro.local>
2020-08-17 14:51:35 +0400

    Added example react project

---
+++ Example/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { LoanApply } from '@rakbank/digital-pl';
+
+function App() {
+  return (
+    <div className="App">
+     <LoanApply />
+    </div>
+  );
+}
+
+export default App;
